6e80b58f1bb651375249446e73b6f52a03387c40
Iida Rinnasto <irinnast@student.oulu.fi>
2019-03-18 17:28:05 +0200

    trying screenManager

--- main.py
+++ main.py
@@ -5,14 +5,26 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
+
+class MenuScreen(Screen):
+    pass
+
+class SeuraavaSivu(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name = 'menu'))
+sm.add_widget(SeuraavaSivu(name = 'next'))
+
 
 class ScatterTextWidget(BoxLayout):
     pass
 
 class Myrkytetty(App):
     def build(self):
-        return ScatterTextWidget()
+        return sm
 
 
 if __name__ == "__main__":
